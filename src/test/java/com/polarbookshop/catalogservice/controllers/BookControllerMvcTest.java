8aeddd400add6d773ae84f6f927f95ecb2e3d704
josousa82 <mfgas2@ou.ac.uk>
2024-08-06 00:35:43 +0100

    Setup github actions.

---
+++ src/test/java/com/polarbookshop/catalogservice/controllers/BookControllerMvcTest.java
@@ -0,0 +1,51 @@
+package com.polarbookshop.catalogservice.controllers;
+
+import com.polarbookshop.catalogservice.domain.BookService;
+import com.polarbookshop.catalogservice.exceptions.BookNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.BDDMockito.given;
+
+// TODO 5. Finish the implementation of tests with WebMvc.
+//  Review concepts and differences from SpringBootTest, such
+//  as WebMvcTest will only start the necessary beans.
+@WebMvcTest(BookController.class)
+class BookControllerMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void whenGetBookingNotExistingThenShouldReturn404() throws Exception {
+        String isbn = "73737313940";
+        given(bookService.viewBookDetails(isbn)).willThrow(BookNotFoundException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/books/" + isbn))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void getBookByIsbn() {
+    }
+
+    @Test
+    void post() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void put() {
+    }
+}
---
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookJsonTest.java
@@ -0,0 +1,51 @@
+package com.polarbookshop.catalogservice.domain;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import org.springframework.boot.test.json.ObjectContent;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@JsonTest
+class BookJsonTest {
+
+    @Autowired
+    private JacksonTester<Book> json;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void testSerialize() throws IOException {
+        var book = new Book("1234567890", "title", "author", 9.90);
+        JsonContent<Book> jsonContent = json.write(book);
+        JsonNode jsonNode = objectMapper.readTree(jsonContent.getJson());
+        assertEquals("1234567890", jsonNode.get("isbn").asText());
+        assertEquals("title", jsonNode.get("title").asText());
+        assertEquals("author", jsonNode.get("author").asText());
+        assertEquals(9.90, jsonNode.get("price").asDouble());
+    }
+
+    @Test
+    void testDeserialize() throws IOException {
+        var content = """
+                        {
+                        "isbn": "1234567890",
+                        "title": "Title",
+                        "author": "Author",
+                        "price": 9.90
+                        }
+                    """;
+        var expectedBook = new Book("1234567890", "Title", "Author", 9.90);
+
+        var jsonContent = json.parse(content);
+        assertThat(jsonContent).usingRecursiveComparison().isEqualTo(expectedBook);
+    }
+}
